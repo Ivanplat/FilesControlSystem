a2cf49d01734369043d3766876d7e1b3306adea8
Ivan Platonov <EventGraphDev@gmail.com>
2023-05-24 20:04:22 +0300

    Tests update and client file moved

--- Source/Tests/test.cpp
+++ Source/Tests/test.cpp
@@ -2,6 +2,9 @@
 #include "Logger.h"
 #include "ServerClientCore.h"
 
+import Server;
+import Client;
+
 TEST(LoggerTests, ExistanceTesting)
 {
 	ASSERT_NE(SLog, nullptr);
@@ -19,6 +22,83 @@ TEST(ServerClientCoreTest, ShuttingdownTest)
 	if (scc.Startup())
 	{
 		ASSERT_EQ(scc.Shutdown(), true);
+	}
+	else
+	{
+		ASSERT_FALSE(true);
+	}
+}
+TEST(ServerClientCoreTest, InitializeTest)
+{
+	ServerClientCore scc;
+	if (scc.Startup())
+	{
+		ASSERT_ANY_THROW(scc.Initialize());
+		scc.Shutdown();
+	}
+	else
+	{
+		ASSERT_FALSE(true);
+	}
+}
+
+TEST(ServerTest, StartingupTest)
+{
+	Server server;
+	ASSERT_EQ(server.Startup(), true);
+}
+
+TEST(ServerTest, ShuttingdownTest)
+{
+	Server server;
+	if (server.Startup())
+	{
+		ASSERT_EQ(server.Shutdown(), true);
+	}
+	else
+	{
+		ASSERT_FALSE(true);
+	}
+}
+TEST(ServerTest, InitializeTest)
+{
+	Server server;
+	if (server.Startup())
+	{
+		ASSERT_NO_THROW(server.Initialize());
+		server.Shutdown();
+	}
+	else
+	{
+		ASSERT_FALSE(true);
+	}
+}
+
+TEST(ClientTest, StartingupTest)
+{
+	Client client;
+	ASSERT_EQ(client.Startup(), true);
+}
+
+TEST(ClientTest, ShuttingdownTest)
+{
+	Client client;
+	if (client.Startup())
+	{
+		ASSERT_EQ(client.Shutdown(), true);
+	}
+	else
+	{
+		ASSERT_FALSE(true);
+	}
+}
+TEST(ClientTest, InitializeTest)
+{
+	Client client;
+	if (client.Startup())
+	{
+		ASSERT_NO_THROW(client.Initialize());
+		client.Shutdown();
 	}
 	else
 	{
